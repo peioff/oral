359bfe66be8075e299b9551ffc2e8c065cff8feb
Poff <p.off.pro@protonmail.com>
2024-07-22 18:12:19 +0200

    main

--- _config.php
+++ _config.php
@@ -1,7 +1,7 @@
 <?php
 #Error managing setup
 ini_set('display_errors', 1);
-error_reporting(E_ALL  & ~E_NOTICE ); // add " & ~E_NOTICE " (To disable notices)
+error_reporting(E_ALL);
 
 /**
  * The AutoLoad Class is used to avoid having to include classes in files each time a class is used
@@ -14,14 +14,13 @@ class AutoLoad
         # autoLoad Function declaration
         spl_autoload_register(array(__CLASS__, 'autoload'));
 
-
         #Get server variables
         $root = $_SERVER['DOCUMENT_ROOT'];
         $host = $_SERVER['HTTP_HOST'];
 
         #Global variables
-        define('HOST', 'https://'.$host.'/app/');
-        define('ROOT', $root.'/app/');
+        define('HOST', 'http://'.$host.'/ecf/');
+        define('ROOT', $root.'/ecf/');
 
         define('CLASSES', ROOT.'classes/');
         define('CONTROLLERS', ROOT.'controllers/');
@@ -31,15 +30,6 @@ class AutoLoad
         define('ASSETSCSS', HOST.'assets/css/');
         define('ASSETSIMG', HOST.'assets/images/');
         define('ASSETSLOGO', HOST.'assets/logos/');
-
-        define('SCRIPTS', HOST.'assets/js/');
-
-        define('HTTP_OK', "200");
-        define('HTTP_BAD_REQUEST', "400");
-        define('HTTP_UNAUTHORIZED', "401");
-        define('HTTP_FORBIDDEN', "403");
-        define('HTTP_NOT_FOUND', "404");
-        define('HTTP_METHOD_NOT_ALLOWED', "405");
     }
 
     #autoLoad function. Avoid to include classes in each objects
