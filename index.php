0f9ddd770d146fd0df2a549633755afd4fe64ccd
peioff <165995780+peioff@users.noreply.github.com>
2024-07-23 10:40:00 +0200

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,13 @@ AutoLoad::start();
 
 echo '<pre>'; print_r($_SERVER); echo '<pre>';
 
+echo '<pre>'; print_r($_GET); echo '<pre>'; echo '<pre>';
+
+echo '<pre>'; print_r($_POST); echo '<pre>';
+
+echo '<pre>'; echo 'host : ' . HOST; echo '<pre>';
+echo '<pre>'; echo 'root : ' . ROOT; echo '<pre>';
+
 #request recovery after being filtered by .htaccess
 $request = "/"; // request set as "/" because no request is being set at the very first launch
 if (isset($_GET['r'])){
