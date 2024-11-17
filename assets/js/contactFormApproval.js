09cd7d4f96eef3465eb78ee920cb66d88d0f2fe1
Poff <p.off.pro@protonmail.com>
2024-11-17 11:20:13 +0100

    update

--- assets/js/contactFormApproval.js
+++ assets/js/contactFormApproval.js
@@ -20,7 +20,7 @@ function formApproval() {
     }
     else {
         $.ajax({
-            url: "https://ecf-arcadia-00d8251bc78c.herokuapp.com/contactApproval",
+            url: "https://oral-56a335cd47f2.herokuapp.com/contactApproval",
                 method: "post",
                 dataType: "json"
                 ,
