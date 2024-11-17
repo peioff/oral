860a8521c9d61dfe541a3c63b39f472492f02a0f
Poff <p.off.pro@protonmail.com>
2024-11-17 12:24:16 +0100

    update

--- views/dashboardContacts.php
+++ views/dashboardContacts.php
@@ -59,7 +59,7 @@ if (!empty($data)) {
                 <p class="contact-content"><?php echo $contact->getContent(); ?></p>
                 <?php if (!$contact->getAnswered()){ ?>
                 <div class="contact-actions">
-                    <a class="view-action" href="mailto:<?php echo $contact->getEmail()?>>">Répondre</a>
+                    <a class="view-action" href="mailto:<?php echo $contact->getEmail()?>">Répondre</a>
                     <a class="view-action--danger" href="<?php echo HOST?>deleteContact/id/<?php echo $contact->getId()?>">Supprimer</a>
                 </div>
                 <?php } else {?>
